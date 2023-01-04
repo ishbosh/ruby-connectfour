c2c0def64dd90cb90da869d8384ab36a1eb031c7
ishbosh <99460383+ishbosh@users.noreply.github.com>
2023-01-04 16:12:09 -0700

    Create #show method and add display support

--- lib/board.rb
+++ lib/board.rb
@@ -2,13 +2,17 @@ require_relative 'display'
 
 class Board
   include DisplayText
-  
+
   def initialize
     @grid = Array.new(6) { Array.new(7) { blank_space } }
   end
 
   def show
-    
+    puts show_column_numbers
+    @grid.each do |row|
+      show_row(row)
+    end
+    puts show_column_numbers
   end
 
   def game_over?
--- lib/display.rb
+++ lib/display.rb
@@ -1,11 +1,24 @@
-require_relative 'board'
-
 module DisplayText
   def show_intro
+    'Connect Four'
   end
 
   def show_input_error
     'Invalid input - try again.'
+  end
+
+  def show_column_numbers
+    ' ' + (1..7).to_a.join(' ')
+  end
+
+  def show_divider
+    '_____________'
+  end
+
+  def show_row(board_row)
+    print '|'
+    print board_row.join('|')
+    puts '|'
   end
 
   def blank_space
@@ -19,4 +32,5 @@ module DisplayText
   def blue_piece
     "\e[34m\u25c9\e[0m"
   end
+
 end
