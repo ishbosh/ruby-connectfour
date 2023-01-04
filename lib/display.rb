f7dcf3d967af07d0974621cea68302e96d143d0d
ishbosh <99460383+ishbosh@users.noreply.github.com>
2023-01-04 15:37:53 -0700

    Add methods for board spaces and player pieces using unicode

--- lib/display.rb
+++ lib/display.rb
@@ -3,4 +3,20 @@ require_relative 'board'
 module DisplayText
   def show_intro
   end
+
+  def show_input_error
+    'Invalid input - try again.'
+  end
+
+  def blank_space
+    "\u25ce"
+  end
+
+  def red_piece
+    "\e[31m\u25c9\e[0m"
+  end
+
+  def blue_piece
+    "\e[34m\u25c9\e[0m"
+  end
 end
