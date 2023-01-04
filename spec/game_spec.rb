a06479d79fb22655884dc4a1d9a6944aa5ca34b6
ishbosh <99460383+ishbosh@users.noreply.github.com>
2023-01-04 15:35:34 -0700

    Add #player_input tests and #update_board test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,6 +10,10 @@ describe Game do
   end
 
   describe '#game_loop' do
+    before do
+      allow(game).to receive(:take_turn)
+    end
+
     context 'when game is over' do
       it 'exits loop' do
         allow(game.board).to receive(:game_over?).and_return true
@@ -56,4 +60,67 @@ describe Game do
     end
   end
 
+  describe '#player_input' do
+    context 'when input is valid' do
+      it 'stops loop and does not display error message' do
+        valid_input = '1'
+        allow(game).to receive(:gets).and_return(valid_input)
+        expect(game).not_to receive(:puts).with(game.show_input_error)
+        game.player_input
+      end
+    end
+
+    context 'when input is invalid' do
+      before do
+        allow(game).to receive(:loop).and_yield
+      end
+
+      it 'shows the input error and does not return anything' do
+        invalid_input = 'z'
+        allow(game).to receive(:gets).and_return(invalid_input)
+        expect(game).to receive(:puts).with(game.show_input_error).once
+        game.player_input
+      end
+    end
+
+    context 'when player inputs incorrect value followed by correct value' do
+      it 'stops the loop after displaying the error message once' do
+        invalid_input = 'a'
+        valid_input = '2'
+        allow(game).to receive(:gets).and_return(invalid_input, valid_input)
+        expect(game).to receive(:puts).with(game.show_input_error).once
+        game.player_input
+      end
+    end
+
+    context 'when player inputs two incorrect values followed by a correct value' do
+      it 'stops the loop after displaying the error message twice' do
+        invalid_one = 'one'
+        invalid_two = '11'
+        valid_input = '7'
+        allow(game).to receive(:gets).and_return(invalid_one, invalid_two, valid_input)
+        expect(game).to receive(:puts).with(game.show_input_error).twice
+        game.player_input
+      end
+    end
+  end
+
+  describe '#verify_input' do
+    # Not necessary to test, the player_input test already tests the return value of this method
+    # and would not pass if this method was not correctly returning valid input.
+  end
+
+  describe '#update_board' do
+    it 'sends update to board with the move' do
+      move = '1'
+      expect(game.board).to receive(:update).with(move)
+      game.update_board(move)
+    end
+  end
 end
+
+
+    # it 'sends show to the board' do
+    #   expect(game.board).to receive(:show)
+    #   game.game_loop
+    # end
