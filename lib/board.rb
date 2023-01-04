6ff22984b5060ac9e7111dc96fa44a370520734e
ishbosh <99460383+ishbosh@users.noreply.github.com>
2023-01-04 15:38:03 -0700

    Create grid of board spaces on initialize

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,10 @@
+require_relative 'display'
+
 class Board
+  include DisplayText
+  
   def initialize
+    @grid = Array.new(6) { Array.new(7) { blank_space } }
   end
 
   def show
@@ -7,5 +12,8 @@ class Board
   end
 
   def game_over?
+  end
+
+  def update(move_column)
   end
 end
